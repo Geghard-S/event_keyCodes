09522936b196e28b625da337499b1f31085fdaf2
Geghard-S <geghard@gmail.com>
2021-05-09 23:57:24 -0700

    second commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+    <div class="key">
+            a
+            <small>event.key</small>
+        </div>
+        <div class="key">
+            65
+            <small>event.keyCode</small>
+        </div>
+        <div class="key">
+            KeyA
+            <small>event.code</small>
+        </div>
+        <div class="key">
+            Press any key
+        </div>
+    `
+})
